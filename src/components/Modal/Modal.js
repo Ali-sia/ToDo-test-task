2e910867d27faaf675e37b509c377afce9101753
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-02-28 22:39:14 +0200

    modal open-close realization

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,37 +1,37 @@
-// import { useEffect } from 'react';
+import { useEffect } from 'react';
 // import css from './Layout.module.css';
 
-// export const Modal = ({ children, onClose }) => {
-//   useEffect(() => {
-//     const handleKeyClose = event => {
-//       if (event.code === 'Escape') {
-//         onClose();
-//       }
-//     };
+export const Modal = ({ children, onClose }) => {
+  useEffect(() => {
+    const handleKeyClose = event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
 
-//     window.addEventListener('keydown', handleKeyClose);
+    window.addEventListener('keydown', handleKeyClose);
 
-//     return () => {
-//       window.removeEventListener('keydown', handleKeyClose);
-//     };
-//   }, [onClose]);
+    return () => {
+      window.removeEventListener('keydown', handleKeyClose);
+    };
+  }, [onClose]);
 
-//   const handleBackdropClick = event => {
-//     if (event.target === event.currentTarget) {
-//       onClose();
-//     }
-//   };
+  const handleBackdropClick = event => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
 
-//   return (
-//     <>
-//       <div className="modal-backdrop" />
-//       <div className="modal" onClick={handleBackdropClick}>
-//         <button type="button" onClick={onClose}>
-//           close
-//         </button>
-//         modal window info
-//         <div className="modal-body">{children}</div>
-//         {/* <div className="modal-dialog modal-dialog-centered">
+  return (
+    <>
+      <div className="modal-backdrop" />
+      <div className="modal" onClick={handleBackdropClick}>
+        <button type="button" onClick={onClose}>
+          close
+        </button>
+        modal window info
+        <div className="modal-body">{children}</div>
+        {/* <div className="modal-dialog modal-dialog-centered">
 //           <div className="modal-content">
 //             <div className="modal-header">
 //               <h5 className="modal-title">Modal title</h5>
@@ -45,7 +45,7 @@
 //             <div className="modal-body">{children}</div>
 //           </div>
 //         </div> */}
-//       </div>
-//     </>
-//   );
-// };
+      </div>
+    </>
+  );
+};
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,18 +1,27 @@
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+
+import { Modal } from '../Modal/Modal';
 import { toggleCompleted } from '../../redux/slice';
 // import css from './Task.module.css';
 
 export const Task = ({ task }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const dispatch = useDispatch();
 
   const handleToggle = () => dispatch(toggleCompleted(task.id));
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
   return (
     <div>
-      <input type="checkbox" checked={task.completed} onChange={handleToggle} />
-      <p>
-        Task: {task.id}, Title: {task.title}, Description: {task.description}
+      {isModalOpen && <Modal onClose={toggleModal}>modal</Modal>}
+
+      <p onClick={toggleModal}>
+        Task: {task.id}, Title: {task.title}
       </p>
+      <input type="checkbox" checked={task.completed} onChange={handleToggle} />
     </div>
   );
 };
