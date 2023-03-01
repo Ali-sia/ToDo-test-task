26dab08427b05fbfad5ef2008f6d785d69fdf197
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-03-01 22:32:41 +0200

    modal fix

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,30 +4,9 @@ import { useEffect } from 'react';
 import css from './Modal.module.css';
 
 export const Modal = ({ children, onClose }) => {
-  useEffect(() => {
-    const handleKeyClose = event => {
-      if (event.code === 'Escape') {
-        onClose();
-      }
-    };
-
-    window.addEventListener('keydown', handleKeyClose);
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyClose);
-    };
-  }, [onClose]);
-
-  const handleBackdropClick = event => {
-    event.preventDefault();
-    if (event.target === event.currentTarget) {
-      onClose();
-    }
-  };
-
   return ReactDOM.createPortal(
     <>
-      <div className={css.backdrop} onClick={handleBackdropClick} />
+      <div className={css.backdrop} />
       <div className={css.modal} onClick={e => e.stopPropagation()}>
         <div>{children}</div>
         <button type="button" onClick={onClose}>
